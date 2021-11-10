d4177c465a312bb502270b3763a8e13b5c57d28e
Nathan Hittesdorf <nathanmhitt@gmail.com>
2021-11-09 19:47:08 -0600

    first

--- SeeGreenApp/ARViewController.swift
+++ SeeGreenApp/ARViewController.swift
@@ -7,23 +7,29 @@
 
 import UIKit
 
-class ARViewController: UIViewController {
-
+class ARViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var plantsArr = ["monsterra", "basil", "fern", "orchid", "succulent"]
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
+        cell?.textLabel!.text = plantsArr[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
 
 }
