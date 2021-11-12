e54d0551a919abcfb61b1e2b55ac2170391ab020
Ara Guevarra <aravilleguevarra@gmail.com>
2021-11-12 17:36:12 -0600

    added round label cocoa touch class to get round buttons, backgrounds, etc.

---
+++ SeeGreenApp/RoundLabel.swift
@@ -0,0 +1,25 @@
+//
+//  RoundLabel.swift
+//  SeeGreenApp
+//
+//  Created by student on 11/12/21.
+//
+
+import UIKit
+
+class RoundLabel: UILabel {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+   override func draw(_ rect: CGRect) {
+        // Drawing code
+        //Use round label instead of user attribute to keep it dynamic, use for backgrounds, buttons, etc.
+       super.draw(rect)
+       self.layer.masksToBounds = true
+       self.layer.cornerRadius = self.bounds.height/2
+       //self.font = UIFont.system
+    }
+    
+
+}
